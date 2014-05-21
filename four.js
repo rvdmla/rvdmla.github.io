4c916dd3138d124654a73e61489e3b2cb340b479
rvdmla <r@rvdm.la>
2014-05-20 19:21:28 -0700

    Banner now moves with parallax too

--- four.js
+++ four.js
@@ -1,10 +1,10 @@
 var scrollOffset;
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 5, window.innerWidth / (window.innerWidth/3), 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 7.5, window.innerWidth / (window.innerWidth/2), 0.1, 1000 );
 
 var renderer = new THREE.CanvasRenderer();
-renderer.setSize( window.innerWidth, window.innerWidth/3 );
+renderer.setSize( window.innerWidth, window.innerWidth/2 );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry(1,1,1);
@@ -21,7 +21,7 @@ var pointLight = new THREE.PointLight( 0xeeffaa, 1, 1 );
 pointLight.position.set( 1,1,3.33 );
 scene.add( pointLight );
 
-camera.position.y = 0.2;
+camera.position.y = 0.1875;
 camera.position.z = 5;
 
 function render() {
@@ -32,6 +32,7 @@ function render() {
   pointLight.intensity = pointLight.position.y = pointLight.position.x = ( 1 - scrollOffset );
   cube.rotation.x = 0.33 - ( scrollOffset / 12 );
   cube.rotation.y = -0.5 + ( scrollOffset / 12 );
+  renderer.domElement.style.webkitTransform = 'translateY(-' + ( window.scrollY / 3 ) + 'px)';
 
   // Render
   renderer.render(scene, camera);
